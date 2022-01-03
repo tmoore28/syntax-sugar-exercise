dcc59bbfd59b9acce21a069f2c37b5f37335e92f
tmoore28 <mooretylor28@gmail.com>
2022-01-02 21:31:52 -0500

    syntax sugar initial commit

---
+++ syntax sugar exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syntax_sugar_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9)? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+        }
+    }
+}
